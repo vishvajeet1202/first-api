30324cbaea89414226283318a195c098199e9f31
Vishvajeet <cvishwajeet1202@gmail.com>
2023-02-03 00:16:40 +0530

    adding response

--- src/main/java/com/avengers/studentManagement/StudentController.java
+++ src/main/java/com/avengers/studentManagement/StudentController.java
@@ -1,6 +1,8 @@
 package com.avengers.studentManagement;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -12,24 +14,30 @@ public class StudentController {
    StudentService studentService;
     //get information
     @GetMapping("/get_student")
-    public Student getStudent(@RequestParam("admnNo") int admnNo){
-
-        return studentService.getStudent(admnNo);
+    public ResponseEntity getStudent(@RequestParam("admnNo") int admnNo){
+        Student response=studentService.getStudent(admnNo);
+        return new ResponseEntity<>(response, HttpStatus.FOUND);
     }
     //adding the information
     @PostMapping("/add_student")
-    public String addStudent(@RequestBody Student student){
-
-        return studentService.addStudent(student);
+    public ResponseEntity addStudent(@RequestBody Student student){
+        String response=studentService.addStudent(student);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
     @PutMapping("/update_student")
-    public String updateStudent(@RequestParam("id") int id, @RequestParam("age") int age){
-
-        return studentService.updateStudent(id,age);
+    public ResponseEntity updateStudent(@RequestParam("id") int id, @RequestParam("age") int age){
+        String response=studentService.updateStudent(id,age);
+        if(response.equals("invalid id")){
+            return new ResponseEntity<>(response,HttpStatus.NOT_MODIFIED);
+        }
+        return new ResponseEntity<>(response,HttpStatus.ACCEPTED);
     }
-    @DeleteMapping("/delete_Student{id}")
-    public String deleteStudent(@PathVariable("id") int admnNo){
-
-            return studentService.deleteStudent(admnNo);
+    @DeleteMapping("/delete_Student/{id}")
+    public ResponseEntity deleteStudent(@PathVariable("id") int admnNo){
+            String response=studentService.deleteStudent(admnNo);
+            if(response.equals("invalid id")){
+                return new ResponseEntity<>(response,HttpStatus.NOT_MODIFIED);
+            }
+            return  new ResponseEntity<>(response,HttpStatus.ACCEPTED);
     }
 }
